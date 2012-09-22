f76964412155e360ab3f76f72111d06279832eb2
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-09-22 09:34:49 +0200

    fluid layout and a table for the jobs

--- src/watchtower/ring.clj
+++ src/watchtower/ring.clj
@@ -10,12 +10,13 @@
   (= "SUCCESS" (:result (:lastBuild job))))
 
 (defn- job-row [job]
-  [:li 
-    (if (successful? job)
-      [:span {:class "label label-success"} "OK"]
-      [:span {:class "label label-important"} "FAIL"])
-    " " 
-    (:name job)])
+  [:tr 
+    [:td
+      (if (successful? job)
+        [:span {:class "btn btn-success"} "OK"]
+        [:span {:class "label label-important"} "FAIL"])]
+    [:td {:width "100%"}
+      (:name job)]])
 
 (defn index []
   (html5 {:lang "en"}
@@ -23,10 +24,12 @@
       [:title "Watchtower"]
       (include-css "/bootstrap/css/bootstrap.min.css")]
     [:body 
-      [:h1 "Watchtower"]
-      [:ul {:class "unstyled"} (map job-row (jenkins/jobs))]
-      [:script {:src "http://code.jquery.com/jquery-latest.js"}]
-      (include-js "/bootstrap/js/bootstrap.min.js")]))
+      [:div {:class "container-fluid"}
+        [:h1 "Watchtower"]
+        [:table {:class "table"}
+          (map job-row (jenkins/jobs))]
+        [:script {:src "http://code.jquery.com/jquery-latest.js"}]
+        (include-js "/bootstrap/js/bootstrap.min.js")]]))
 
 
 (defroutes app-routes
