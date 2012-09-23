3200d78a6a068fb404e4302a1b7ac420b8c72d7c
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-09-23 10:04:22 +0200

    added some bootstrap themes and a theme selector

--- src/watchtower/ring.clj
+++ src/watchtower/ring.clj
@@ -1,4 +1,5 @@
 (ns watchtower.ring
+  (:use clojure.java.io)
   (:use compojure.core)
   (:use hiccup.core)
   (:use hiccup.page)
@@ -13,27 +14,45 @@
   [:tr 
     [:td
       (if (successful? job)
-        [:span {:class "btn btn-success"} "OK"]
-        [:span {:class "label label-important"} "FAIL"])]
+        [:span {:class "btn btn-large btn-success"} "OK"]
+        [:span {:class "btn btn-large btn-danger"} "FAIL"])]
     [:td {:width "100%"}
-      (:name job)]])
+      [:h4 (:name job)]]])
 
-(defn index []
-  (html5 {:lang "en"}
-    [:head
-      [:title "Watchtower"]
-      (include-css "/bootstrap/css/bootstrap.min.css")]
-    [:body 
-      [:div {:class "container-fluid"}
-        [:h1 "Watchtower"]
-        [:table {:class "table"}
-          (map job-row (jenkins/jobs))]
-        [:script {:src "http://code.jquery.com/jquery-latest.js"}]
-        (include-js "/bootstrap/js/bootstrap.min.js")]]))
+(defn- themes []
+  (seq (.list (file "resources/public/bootstrap/themes"))))
+
+(defn- theme-selector [theme]
+  [:div {:class "btn-group"}
+    [:a {:class "btn dropdown-toggle" :data-toggle "dropdown" :href "#"}
+      "Change Theme"
+      [:span {:class "caret"}]]
+    [:ul {:class "dropdown-menu"}
+      (map (fn [t] [:a {:href (str "?theme=" t)} t]) (themes))]])
+
+;  [:span {:class "btn btn-large btn-success"} current])
+
+(defn index [theme]
+  (let [theme (if (empty? theme) "slate" theme)]
+    (html5 {:lang "en"}
+      [:head
+        [:title "Watchtower"]
+        (include-css (str "/bootstrap/themes/" theme "/bootstrap.min.css"))]
+      [:body 
+        [:div {:class "container-fluid"}
+          [:div {:class "row-fluid"}
+            [:div {:class "span10"}
+              [:h1 "Watchtower"]]
+            [:div {:class "span2"}
+              (theme-selector theme)]]
+          [:table {:class "table"}
+            (map job-row (jenkins/jobs))]
+          [:script {:src "http://code.jquery.com/jquery-latest.js"}]
+          (include-js "/bootstrap/js/bootstrap.min.js")]])))
 
 
 (defroutes app-routes
-  (GET "/" [] (index))
+  (GET "/" [theme] (index theme))
   (route/resources "/")
   (route/not-found "Not Found"))
 
